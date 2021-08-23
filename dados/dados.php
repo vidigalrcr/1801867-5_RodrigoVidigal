0cdfef29765e6d43a5f9c61219c92ee7d5667bc5
Rodrigo de Castro Vidigal <86722790+vidigalrcr@users.noreply.github.com>
2021-08-23 10:03:01 -0300

    Add files via upload

---
+++ dados/dados.php
@@ -0,0 +1,67 @@
+<?php
+	
+	$home = [
+		'titulo' => "Quando fazemos com carinho, dedicação e amor, o reconhecimento sempre vem!",
+		0 => [
+			"titulo" => "cupcake",
+			"info" => "Uma grande variedade de Cupcakes de dar água na boca"],
+		1 => [
+			"titulo" => "torta alemã",
+			"info" => "Detalhes fazem a diferença em momentos especiais."],
+		2 => [
+			"titulo" => "brigadeiro gourmet",
+			"info" => "Cremosa e com sabor intenso de chocolate, feito com os melhores produtos"],
+		3 => [
+			"titulo" => "bolos especiais",
+			"info" => "Bolos especiais e decorados. Sua imaginação é o limite!"],
+	];
+
+	$quemsomos = [
+		'titulo' => 'Um pouco sobre nós',
+		'info' => "<p>A Doce Sabor faz parte do nosso dia a dia, é a nossa realidade, nossa grande paixão!</p><p>Todos os dias nos apaixonamos por esse trabalho!</p>
+		<p>Tudo o que fazemos com amor e carinho fica uma delícia. E os nossos cupcakes, bolos, doces e tortas estão recheados com todo esse amor.</p>
+		<p>Esse é o nosso diferencial, além de produtos naturais e de qualidade, cada receita tem sua característica própria, mas todas têm uma qualidade em comum, o carinho que colocamos em cada produto!!!</p>
+		<p>A Doce Sabor vem nos proporcionando um dos melhores sentimentos da vida, a felicidade de poder transmitir a cada consumidor a nossa paixão pela confeitaria!</p>"
+	];
+
+	$produtos = [
+		"cupcake" => [
+			"titulo" => "cupcakes",
+			"info" => "<p>Bateu aquela vontade de comer um doce de qualidade? Aqui na Doce Sabor você vai se deliciar.</p>
+						<p>Experimente os nossos deliciosos CUPCAKES.</p>
+						<p>Cupcake no tamanho tradicional (aproximadamente 7cm de diâmetro) com massa nos sabores chocolate, baunilha ou cenoura e coberturas nos sabores ganache de chocolate meio amargo, brigadeiro branco ou preto, doce de leite ou chantilly (que pode ser tingido em cores variadas).</p>
+						<p>Recheios personalizados e confeitos diversos. Quantidade mínima do pedido: 15 cupcakes por sabor.</p>",
+			"valor" => 7.00],
+		
+		"torta" => [
+			"titulo" => "torta alemã",
+			"info" => "<p>Detalhes fazem a diferença em momentos especiais.</p>
+						<p>Bons momentos da vida merecem ser apreciados com sabores inesquecíveis ao lado de quem a gente ama!</p>
+						<p>A cada pedaço, uma experiência incrível!</p>
+						<p></p>",
+			"valor" => 9.00],
+		
+		"brigadeiro" => [
+			"titulo" => "brigadeiro gourmet",
+			"info" => "<p>Não dá para se pensar em brigadeiro e logo não sentir aquela felicidade!</p>
+						<p>O brigadeiro gourmet é uma versão sofisticada do brigadeiro comum. Este novo conceito se destaca na seleção de produtos de alta qualidade, receitas individuais e combinações de ingredientes inusitados.</p>
+						<p>Nossos brigadeiros são produzidos artesanalmente com ingredientes selecionados, chocolates importados e receitas exclusivas. Tudo isso para garantir um sabor irresistível.</p>",
+			"valor" => 5.00],
+		
+		"bolo" => [
+			"titulo" => "bolos especiais",
+			"info" => "<p>O bolo representa aquele momento em que as pessoas se unem ao redor dos anfitriões para celebrar um evento muito especial.</p>
+						<p>Eles são feitos com 4 camadas de bolo e 3 de recheio. Você pode pedir pelo nosso delivery, os sabores à pronta entrega ou você pode solicitar uma decoração personalizada.</p>
+						<p>Nossos bolos são feitos, sempre, com os melhores ingredientes.</p>",
+			"valor" => 60.00]
+		];
+
+	$rodape = [
+		"info" => "<p>Desenvolvido por: Rodrigo de Castro Vidigal</p>
+		<p>curso: SUPERIOR DE TECNOLOGIA EM SISTEMAS PARA INTERNET</p>
+		<p>RA: 1801867-5</p>"
+	];
+
+
+?>
+
